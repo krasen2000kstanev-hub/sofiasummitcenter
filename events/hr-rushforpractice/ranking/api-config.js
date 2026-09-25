704fb7e86d919d384c2763c378624c9795cf4699
krasen2000kstanev-hub <krasen2000.k.stanev@gmail.com>
2026-09-25 11:24:57 +0300

    fix: configure ranking admin auth

--- events/hr-rushforpractice/ranking/api-config.js
+++ events/hr-rushforpractice/ranking/api-config.js
@@ -1,3 +1,3 @@
-window.__LEADERBOARD_API__='';
-window.__COGNITO_CLIENT_ID__='';
-window.__AWS_REGION__='';
+window.__LEADERBOARD_API__='https://sofiasummit-events-api.krasen2000-k-stanev.workers.dev';
+window.__COGNITO_CLIENT_ID__='6bvki55do8vt4vmp98nhnglhlo';
+window.__AWS_REGION__='us-east-1';
